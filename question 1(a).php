69fea3fe3a4011ede84878a64b3393192b69086e
Emmanuel Mailu <emmanuel.k.mailu20@gmail.com>
2023-02-13 14:49:33 +0300

    deleted a file . renamed question 1 from assignment 1 to question 1

--- question 1(a).php
+++ question 1(a).php
@@ -1,7 +1,7 @@
 <?php
 
 ECHO "My Name is <u>Peter</u><br><br>";
-echo "<i>I am a kenyan<br></i><br>";
+print "<i>I am a kenyan<br></i><br>";
 EcHo "<b>My proffesion is PHP Programmer <br></b>";
 
 ?>
