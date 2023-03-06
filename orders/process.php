e981fabaa38f794f4bd5ac1b973fb911b32bcf84
Emmanuel Mailu <emmanuel.k.mailu20@gmail.com>
2023-03-06 10:41:28 +0300

    processs

---
+++ orders/process.php
@@ -0,0 +1,14 @@
+
+
+<?php
+
+require_once('dbconnect.php');
+
+
+
+// insert details into database 
+$insert = "INSERT INTO orders(customer,product,quantity,price,total)"
+            values('customer','product','quantity','price','totalcost')";
+        
+
+?>
